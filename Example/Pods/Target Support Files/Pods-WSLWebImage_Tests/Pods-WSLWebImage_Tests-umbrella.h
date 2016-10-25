e084ae5c4f8aec9769d0dc785a7b6a0aeacecda2
slwang613928 <865499908@qq.com>
2016-10-25 15:01:54 +0800

    Initial Commit of Library
    Signed-off-by: slwang613928 <865499908@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-WSLWebImage_Tests/Pods-WSLWebImage_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_WSLWebImage_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WSLWebImage_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/WSLWebImage/WSLWebImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSLWebImage : NSObject
+@end
+@implementation PodsDummy_WSLWebImage
+@end
