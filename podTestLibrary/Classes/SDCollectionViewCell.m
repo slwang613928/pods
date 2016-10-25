339c83fd482901e510a4ecbe1540789da07ad358
slwang613928 <865499908@qq.com>
2016-10-25 10:39:13 +0800

    删除不要的文件

--- podTestLibrary/Classes/SDCollectionViewCell.m
+++ podTestLibrary/Classes/SDCollectionViewCell.m
@@ -31,87 +31,14 @@
 
 
 #import "SDCollectionViewCell.h"
-#import "UIView+SDExtension.h"
+
 
 @implementation SDCollectionViewCell
 {
-    __weak UILabel *_titleLabel;
+  
 }
 
 
-- (instancetype)initWithFrame:(CGRect)frame
-{
-    if (self = [super initWithFrame:frame]) {
-        [self setupImageView];
-        [self setupTitleLabel];
-    }
-    
-    return self;
-}
 
-- (void)setTitleLabelBackgroundColor:(UIColor *)titleLabelBackgroundColor
-{
-    _titleLabelBackgroundColor = titleLabelBackgroundColor;
-    _titleLabel.backgroundColor = titleLabelBackgroundColor;
-}
-
-- (void)setTitleLabelTextColor:(UIColor *)titleLabelTextColor
-{
-    _titleLabelTextColor = titleLabelTextColor;
-    _titleLabel.textColor = titleLabelTextColor;
-}
-
-- (void)setTitleLabelTextFont:(UIFont *)titleLabelTextFont
-{
-    _titleLabelTextFont = titleLabelTextFont;
-    _titleLabel.font = titleLabelTextFont;
-}
-
-- (void)setupImageView
-{
-    UIImageView *imageView = [[UIImageView alloc] init];
-    _imageView = imageView;
-    _imageView.contentMode =  UIViewContentModeScaleAspectFill;
-    _imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
-    _imageView.clipsToBounds  = YES;
-    [self.contentView addSubview:imageView];
-}
-
-- (void)setupTitleLabel
-{
-    UILabel *titleLabel = [[UILabel alloc] init];
-    _titleLabel = titleLabel;
-    _titleLabel.hidden = YES;
-    [self.contentView addSubview:titleLabel];
-}
-
-- (void)setTitle:(NSString *)title
-{
-    _title = [title copy];
-    _titleLabel.text = [NSString stringWithFormat:@"   %@", title];
-    if (_titleLabel.hidden) {
-        _titleLabel.hidden = NO;
-    }
-}
-
-
-- (void)layoutSubviews
-{
-    [super layoutSubviews];
-    
-    if (self.onlyDisplayText) {
-        _titleLabel.frame = self.bounds;
-    } else {
-        _imageView.frame = self.bounds;
-        _imageView.contentMode =  UIViewContentModeScaleAspectFill;
-        _imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
-        _imageView.clipsToBounds  = YES;
-        CGFloat titleLabelW = self.sd_width;
-        CGFloat titleLabelH = _titleLabelHeight;
-        CGFloat titleLabelX = 0;
-        CGFloat titleLabelY = self.sd_height - titleLabelH;
-        _titleLabel.frame = CGRectMake(titleLabelX, titleLabelY, titleLabelW, titleLabelH);
-    }
-}
 
 @end
