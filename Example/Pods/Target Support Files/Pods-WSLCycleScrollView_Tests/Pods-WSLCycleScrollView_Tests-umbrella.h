cc75afd011e8eb9065bc34437f6426e2f5458f6f
slwang613928 <865499908@qq.com>
2016-10-26 13:46:38 +0800

    Initial Commit of Library
    Signed-off-by: slwang613928 <865499908@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-WSLCycleScrollView_Tests/Pods-WSLCycleScrollView_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_WSLCycleScrollView_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WSLCycleScrollView_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/WSLCycleScrollView/WSLCycleScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSLCycleScrollView : NSObject
+@end
+@implementation PodsDummy_WSLCycleScrollView
+@end
